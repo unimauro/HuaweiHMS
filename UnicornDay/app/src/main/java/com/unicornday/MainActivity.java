f0ca27b2e4f00468667fb0fb7b86b78b20923cfd
Unimauo <unimauro@gmail.com>
2020-06-13 13:12:44 -0500

    Enviando

---
+++ UnicornDay/app/src/main/java/com/unicornday/MainActivity.java
@@ -0,0 +1,94 @@
+package com.unicornday;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+import android.util.Log;
+
+import com.huawei.hms.maps.HuaweiMap;
+import com.huawei.hms.maps.MapView;
+import com.huawei.hms.maps.OnMapReadyCallback;
+import com.huawei.hms.maps.util.LogM;
+
+/**
+ * map activity entrance class
+ */
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private static final String TAG = "MapViewDemoActivity";
+    //HUAWEI map
+    private HuaweiMap hMap;
+
+    private MapView mMapView;
+
+    private static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        LogM.d(TAG, "onCreate:hzj");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //get mapview instance
+        mMapView = findViewById(R.id.mapView);
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
+        }
+        mMapView.onCreate(mapViewBundle);
+        //get map instance
+        mMapView.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(HuaweiMap map) {
+        //get map instance in a callback method
+        Log.d(TAG, "onMapReady: ");
+        hMap = map;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mMapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mMapView.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        mMapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    private static boolean hasPermissions(Context context, String... permissions) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
